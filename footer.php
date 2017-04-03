b1aaa67ee9b391e22f3293c9301908af49526f79
Keith Senkowski <keith.senkowski@gmail.com>
2017-04-02 20:52:51 -0500

    created footer base structure

--- footer.php
+++ footer.php
@@ -1,7 +1,22 @@
 <footer class="footer" role="contentinfo">
-	<div class="content">
-	<p class="copyright">&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?>.</p>
-</div>
+	<div class="content gutters">
+		<div class="col span-2">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="logo">
+		</div>
+		<div class="col span-8">
+			<ul class="unstyled">
+				<li>M.A.S.K., P.O. BOX 368755,</li>
+				<li>Chicago, Illinois 60636</li>
+				<li></li>
+				<li class="phone"><a href="tel:7738859871">773.885.9871</a></li>
+				<li class="mail"><a href="mailto:administrator@getbehindthemask.org">administrator@getbehindthemask.org</a></li>
+			</ul>
+		</div>
+		<div class="col span-2">
+			<div class="social-group"></div>
+			<a class="button secondary-light-bg">Download Press Kit</a>
+		</div>
+	</div>
 </footer>
 
 		<?php wp_footer(); ?>
