749f35315f2ee978e539e2446857634beeaf9c96
Keith Senkowski <keith.senkowski@gmail.com>
2017-04-09 16:49:17 -0500

    about page qa updates

--- wp-content/themes/mask/template-about.php
+++ wp-content/themes/mask/template-about.php
@@ -9,7 +9,6 @@ $armySubtitle = get_field('army_subtitle');
 $armyCopy = get_field('army_copy');
 
 $heroBG = 'background:linear-gradient(rgba(69, 96, 85, 0.7),rgba(69, 96, 85, 0.7)),url('.$heroImage.') top center / cover no-repeat fixed;';
-$armyBG = 'background:linear-gradient(rgba(69, 96, 85, 0.7),rgba(69, 96, 85, 0.7)),url('.$armyImage.') top center / cover no-repeat;';
 ?>
 
 	<main role="main">
@@ -28,7 +27,7 @@ $armyBG = 'background:linear-gradient(rgba(69, 96, 85, 0.7),rgba(69, 96, 85, 0.7
 			<section class="what-we-do header-sync">
 				<div class="top"></div>
 				<div class="content gutters">
-					<div class="span-6 col">
+					<div class="span-6 col left">
 						<h2>
 							<?php
 							if($whatTitle){
@@ -42,17 +41,26 @@ $armyBG = 'background:linear-gradient(rgba(69, 96, 85, 0.7),rgba(69, 96, 85, 0.7
 						if($whatLeft){
 							echo $whatLeft;
 						}else{
-							echo 'A bunch of copy';
+							echo '<p>MASK (Mothers/Men Against Senseless Killings) was
+established as a way to put eyes in the streets,
+interrupt violence and crime, and teach children to
+grow up as friends rather than enemies. A group of
+caring individuals in the community began to simply
+hang out on the block, cook food, and emanate love.</p><p>Our presence was felt. People began to notice
+neighbors were watching out for each other,
+and it was contagious. Now this method of
+injecting good vibes in troubled areas
+is catching on in more communities.</p>';
 						}
 						?>
 						
 					</div>
-					<div class="span-6 col">
+					<div class="span-6 col right">
 						<?php
 						if($whatLeft){
 							echo $whatRight;
 						}else{
-							echo 'A bunch of copy';
+							echo '<p>Our primary mission is to build stronger communities through a focus on:</p><ul><li>Violence Prevention</li><li>Food Insecurity</li><li>Housing</li></ul><p>Additionally, MASK partners to ensure that community members have access to necessary city services, opportunities for education & professional skills growth, and economic development.</p>';
 						}
 						?>
 						
@@ -87,8 +95,8 @@ $armyBG = 'background:linear-gradient(rgba(69, 96, 85, 0.7),rgba(69, 96, 85, 0.7
 						if($armyCopy){
 							echo $armyCopy;
 						}else{
-							echo 'I used to think my greatest accomplishment was raising two happy, healthy children in Chicago, where so many other mothers are denied that right. Then I sat in a lawn chair on a street corner and extended the love I have for my kids to someone else’s. I have been enriched and deeply fulfilled by all of my children. I hope that one day you get to experience the same level of purpose that I have.
-See you on the block.';
+							echo '<p>I used to think my greatest accomplishment was raising two happy, healthy children in Chicago, where so many other mothers are denied that right. Then I sat in a lawn chair on a street corner and extended the love I have for my kids to someone else’s. I have been enriched and deeply fulfilled by all of my children. I hope that one day you get to experience the same level of purpose that I have.
+See you on the block.</p>';
 						}
 						?>
 						<img src="<?php echo get_template_directory_uri(); ?>/dist/img/signature.svg" onerror="this.src='<?php echo get_template_directory_uri(); ?>/dist/img/signature.png'" class="signature">
